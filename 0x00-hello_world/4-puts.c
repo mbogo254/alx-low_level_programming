e42803e52959480537aeae1d5512876134689bb4
mbogo254 <samuelmbogo26@gmail.com>
2022-06-30 14:55:35 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - print the string in the put function
+ *
+ * return: success
+ */
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
