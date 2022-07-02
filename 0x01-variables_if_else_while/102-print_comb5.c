0ec7f8da51b578b161a4eca644df035c71d9dd05
mbogo254 <samuelmbogo26@gmail.com>
2022-07-01 17:58:30 -0700

    variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,23 +7,27 @@
  */
 int main(void)
 {
-int ch;
-int n;
+int i, j;
 
-for (ch = 48; ch <= 57; ch++)
+for (i = 0; i < 100; i++)
 {
-for (n = 48; n <= 57; n++)
+for (j = 0; j < 100; j++)
 {
-putchar(ch);
-putchar(n);
-if (ch != 57 || n != 57)
+if (i < j)
 {
-putchar(44);
-putchar(32);
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
 }
 }
 }
-
+}
 putchar('\n');
 return (0);
 }
