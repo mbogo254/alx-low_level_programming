084db7ab2d133761a200f5d5f355fd50b33fa1ec
mbogo254 <samuelmbogo26@gmail.com>
2022-07-05 13:30:15 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - return 1 if c is a letter
+ *
+ * @c: the int to print
+ * Return:0
+ */
+
+int _isalpha(int c)
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
