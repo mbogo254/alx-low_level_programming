d373261ebfdc9fa28e841f7852d84aa61ea56200
mbogo254 <samuelmbogo26@gmail.com>
2022-08-02 07:39:00 -0700

    0D preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file of the program
+ * was compiled from, followed by new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", _file_);
+
+	return (0);
+}
